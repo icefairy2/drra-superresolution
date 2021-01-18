af1f2a625e2b7072511d0f88a8baee9ddd292d5a
Gyarmathy Timea <gyarmathy.timi@hotmail.com>
2021-01-18 11:55:42 +0100

    Added better parameter parsing, model saving and loading, postprocessing and model files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import getopt
 import os
 import sys
 
@@ -6,16 +7,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Conv2D
-from tensorflow.python.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.python.keras.utils.vis_utils import plot_model
 
 from adamlrm import AdamLRM
 
 # Constants for training hyperparameters
 
-DEFAULT_IMAGE_SIZE = 64
+DEFAULT_IMAGE_SIZE = 512
 
-NR_EPOCHS = 3
+NR_EPOCHS = 100
 NR_IMAGES_PER_BATCH = 10
 VALIDATION_SPLIT = 0.2
 
@@ -45,16 +46,16 @@ LOW_RES_DATA_PATH_TEST_DIFFICULT = "data/DIV2K_valid_LR_difficult"
 # http://mmlab.ie.cuhk.edu.hk/projects/SRCNN.html
 SRCNN_modified = Sequential()
 
-SRCNN_modified.add(Conv2D(filters=128, kernel_size=(9, 9), activation='relu', padding="same", use_bias=True,
+SRCNN_modified.add(Conv2D(filters=256, kernel_size=(9, 9), activation='relu', padding="same", use_bias=True,
                           input_shape=(DEFAULT_IMAGE_SIZE, DEFAULT_IMAGE_SIZE, 3), name="layer1"))
 SRCNN_modified.add(
-    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding="same", use_bias=True, name="layer2"))
+    Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding="same", use_bias=True, name="layer2"))
 SRCNN_modified.add(
-    Conv2D(filters=3, kernel_size=(5, 5), activation='relu', padding="same", use_bias=True, name="layer3"))
+    Conv2D(filters=3, kernel_size=(5, 5), padding="same", use_bias=True, name="layer3"))
 
 # We implemented variable learning rates per layer in order to try to achieve good performance
 lr_multiplier = {
-    'layer1': 0.001,
+    'layer1': 0.0001,
     'layer2': 0.0001,
     'layer3': 0.00001
 }
@@ -86,13 +87,13 @@ def define_low_res_paths(type):
     if type is None or type == ARG_X8:
         print("Using bicubic x8 downscaling...")
         return LOW_RES_DATA_PATH_TRAIN_X8, LOW_RES_DATA_PATH_TEST_X8
-    if type == ARG_MILD:
+    elif type == ARG_MILD:
         print("Using realistic mild x4 downscaling...")
         return LOW_RES_DATA_PATH_TRAIN_MILD, LOW_RES_DATA_PATH_TEST_MILD
-    if type == ARG_WILD:
+    elif type == ARG_WILD:
         print("Using realistic wild x4 downscaling...")
         return LOW_RES_DATA_PATH_TRAIN_WILD, LOW_RES_DATA_PATH_TEST_WILD
-    if type == ARG_DIFFICULT:
+    elif type == ARG_DIFFICULT:
         print("Using realistic difficult x4 downscaling...")
         return LOW_RES_DATA_PATH_TRAIN_DIFFICULT, LOW_RES_DATA_PATH_TEST_DIFFICULT
 
@@ -136,7 +137,7 @@ def compute_psnr(input_img, label):
     input_data = input_img.astype(float)
     label_data = label.astype(float)
 
-    diff = np.mean((input_data - label_data)**2.)
+    diff = np.mean((input_data - label_data) ** 2.)
     if diff == 0:
         return 100
     rmse = math.sqrt(np.mean(diff ** 2.))
@@ -145,10 +146,15 @@ def compute_psnr(input_img, label):
 
 if __name__ == "__main__":
     low_res_type = ARG_X8
+    is_load_model = False
 
     # Read command line arguments
-    if len(sys.argv) > 0:
-        low_res_type = sys.argv[1]
+    opts, args = getopt.getopt(sys.argv[1:], "c:l", ["load_model", "category"])
+    for opt, arg in opts:
+        if opt in ("-c", "--category"):
+            low_res_type = arg
+        elif opt in ("-l", "--load_model"):
+            is_load_model = True
 
     low_res_train_path, low_res_test_path = define_low_res_paths(low_res_type)
 
@@ -167,8 +173,19 @@ if __name__ == "__main__":
     low_res_array_test = np.divide(np.array(low_res_test_data), 255, dtype='float32')
     high_res_array_test = np.divide(np.array(high_res_test_data), 255, dtype='float32')
 
-    print("Training model...")
-    train_model(high_res_array_train, low_res_array_train)
+    print("Training data shape:")
+    print(high_res_array_train.shape)
+
+    if is_load_model:
+        print("Loading model...")
+        SRCNN_modified.load_weights('trained_model.h5')
+    else:
+        print("Training model...")
+        SRCNN_modified.load_weights('trained_model.h5')
+        train_model(high_res_array_train, low_res_array_train)
+
+        print("Saving model...")
+        SRCNN_modified.save('trained_model.h5')
 
     print("Compute the PSNR between each LR image and its prediction...")
     prediction = SRCNN_modified.predict(low_res_array_test)
@@ -186,21 +203,29 @@ if __name__ == "__main__":
     plt.plot(bicubic_scores, label="Bicubic PSNR")
     plt.show()
 
-    print("Predicting the image with the minimum PSNR score...".format(predicted_scores[np.argmin(predicted_scores)]))
     image_index = np.argmin(predicted_scores)
+    print("Predicting the image {0} with the minimum PSNR score {1}...".format(image_index,
+                                                                               predicted_scores[image_index]))
     min_image_lr = np.array([low_res_array_test[image_index]])
     predicted_min_image = SRCNN_modified.predict(min_image_lr)
+    predicted_min_image = predicted_min_image[0]
+    predicted_min_image[np.where(predicted_min_image < 0)] = 0
+    predicted_min_image[np.where(predicted_min_image > 1)] = 1
     predicted_min_image *= 255
 
     # Visualize example image
-    cv2.imshow('Low resolution image with min PSNR', low_res_test_data[image_index])
-    cv2.imshow('High resolution image with min PSNR', high_res_test_data[image_index])
-    cv2.imshow('Predicted image with min PSNR', predicted_min_image[0].astype('uint8'))
+    cv2.imshow('Low resolution image', low_res_test_data[image_index])
+    cv2.imshow('High resolution image', high_res_test_data[image_index])
+    cv2.imshow('Predicted image', predicted_min_image.astype('uint8'))
 
-    print("Predicting the image with the highest PSNR score...".format(predicted_scores[np.argmax(predicted_scores)]))
     image_index = np.argmax(predicted_scores)
+    print("Predicting the image {0} with the highest PSNR score {1}...".format(image_index,
+                                                                               predicted_scores[image_index]))
     max_image_lr = np.array([low_res_array_test[image_index]])
     predicted_max_image = SRCNN_modified.predict(max_image_lr)
+    predicted_max_image = predicted_max_image[0]
+    predicted_max_image[np.where(predicted_max_image < 0)] = 0
+    predicted_max_image[np.where(predicted_max_image > 1)] = 1
     predicted_max_image *= 255
 
     # Visualize example image
